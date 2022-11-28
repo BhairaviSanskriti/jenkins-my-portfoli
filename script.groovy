f308788ec57e7475aed8bbdfc26a9006379d4f98
Sanskriti <bhairavi.sanskriti@gmail.com>
2022-11-29 00:21:16 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,9 +1,9 @@
 def buildImage(){
   echo "Image is being built with version ${param.VERSION}.${BUILD_NUMBER} ..."
   withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PSW', usernameVariable: 'USER')]) {
-    sh "docker build -t bhairavisanskriti/sanskriti-website:${VERSION}.${BUILD_NUMBER}"
+    sh "docker build -t bhairavisanskriti/sanskriti-website:${param.VERSION}.${BUILD_NUMBER}"
     sh "echo ${PSW} | docker login -u ${USER} --password-stdin"
-    sh "docker push bhairavisanskriti/sanskriti-website:${VERSION}.${BUILD_NUMBER}"
+    sh "docker push bhairavisanskriti/sanskriti-website:${param.VERSION}.${BUILD_NUMBER}"
   }
   echo "Image is pushed to DockerHub."
 }
@@ -14,4 +14,15 @@ def testApp(){
 
 def deployApp(){
   echo "Deploying application..."
+}
+
+def commitChanges(){
+  withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'PSW', usernameVariable: 'USER')]) {
+    sh 'git config --global user.name "jenkins"'
+    sh 'git config --global user.email "my.jenkins.server@gmail.com"'
+    sh "git remote set-url origin https://${USER}:${PSW}@github.com/BhairaviSanskriti/jenkins-my-portfolio.git"
+    sh "git add ."
+    sh 'git commit -m "updated version"'
+    sh "git push origin HEAD:${BRANCH_NAME}"
+  }
 }
