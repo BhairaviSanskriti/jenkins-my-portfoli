804ddd47f86dbff11aa341c28d803a36c7c04075
Sanskriti <bhairavi.sanskriti@gmail.com>
2022-11-29 17:12:27 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -23,7 +23,7 @@ def commitChanges(){
     sh "git remote set-url origin https://${USER}:${PSW}@github.com/BhairaviSanskriti/jenkins-my-portfolio.git"
     
     sh '''#!/bin/bash
-         sed -i 's/Version:.*/Version: '"${BUILDNUMBER}"'/g' index.html
+         sed -i 's/Version:.*/Version: '"${BUIL_DNUMBER}"'/g' index.html
     '''
     
     sh "git add ."
